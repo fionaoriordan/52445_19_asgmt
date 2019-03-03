53a7ef147e166f8c90426f6a0e1ad73b693a1fa9
Fiona O'Riordan <fiona_oriordan@hotmail.com>
2019-03-03 16:56:53 +0000

    First Draft of squareroot.py created.

---
+++ squareroot.py
@@ -0,0 +1,11 @@
+# # Fiona O'Riordan, 03 Mar 2019
+# Write a program that that takes a positive floating point number as input and outputs 
+# an approximation of its square root.
+
+
+import math
+num = float(input('Please enter a positive floating number:' ))
+
+print(round(math.sqrt(num),1))
+
+ 
