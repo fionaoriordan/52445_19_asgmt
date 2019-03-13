3bb1ca11136a501d06aa20a56fe6a218b127f698
Fiona O'Riordan <fiona_oriordan@hotmail.com>
2019-03-13 18:17:25 +0000

    fionadatetime.py to include a function to calculate the suffix for the date and include this in the output. Also lower case am/pm included.

--- fionadatetime.py
+++ fionadatetime.py
@@ -1,20 +1,49 @@
 # Fiona O'Riordan, Created 3-Mar-19
 #Solution to Question 8
-# Write a program that outputs today’s date and time in the format ”Monday, January 10th 2019 at 1:15pm”.
+# Write a program that outputs today’s date and time in the format
+#  ”Monday, January 10th 2019 at 1:15pm”.
 # Adapted from Python Tutorial 10.8. Dates and Times https://docs.python.org/3/tutorial/stdlib.html#dates-and-times
 # and https://stackoverflow.com/questions/46504954/converting-time-to-am-pm-using-python
-from datetime import datetime
 
-rightnow = datetime.now()
+from datetime import datetime 
 
-print(rightnow.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B."))
-print(rightnow.strftime("%A, %B %d'something' %Y at %H:%M %p"))
+# rightnow = datetime.now()
+
+# print(rightnow.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B."))
+# print(rightnow.strftime("%A, %B %d'something' %Y at %H:%M %p"))
+
+# Creating a function which reads an integer value and outputs the value 
+# as a string with appropriate suffix st, nd, rd or th
+def nth(day):
+# set suffix = 'th' for all days btwn 11 and 20
+    if day > 10 and day < 20: 
+        suffix = 'th'
+# set suffix for all other numbers based on 
+# thelast digit in the day
+    else:   
+        unit = str(day)
+        unit = unit[-1]
+        if unit == "1": 
+            suffix = 'st'
+        elif unit == "2":
+            suffix = 'nd'
+        elif unit == "3":
+            suffix = 'rd'
+        else:
+            suffix = 'th'
+    return str(day)+ suffix
+
+# dayord is todays date in the format nth e.g. 25th
+dayord = nth(int(datetime.now().strftime("%d")))
+daymonth = datetime.now().strftime("%A, %B") 
+year = datetime.now().strftime("%Y") 
+time = datetime.now().strftime("%-I:%M")
+ampm = datetime.now().strftime("%p").lower()
+
+print(f"{daymonth} {dayord} {year} at {time}{ampm}")
 
 
-# Open issues Monday 04th March-2019
-# 1. Need to include way to return st, nd, rd, th depending on the value of %d
-# 2. Need to include way to return pm instead of PM and similarly with AM
-# 3. Condense code
+
 
 
 
