d22f6a02108a12bf4def7f6a6316ba0e5c8cbc9f
Fiona O'Riordan <fiona_oriordan@hotmail.com>
2019-03-18 17:43:02 +0000

    updated comments on fionadatetime.py to record that earlier error with name file may not have been an issue.

--- fionadatetime.py
+++ fionadatetime.py
@@ -2,15 +2,13 @@
 #Solution to Question 8
 # Write a program that outputs today’s date and time in the format
 #  ”Monday, January 10th 2019 at 1:15pm”.
-# Adapted from Python Tutorial 10.8. Dates and Times https://docs.python.org/3/tutorial/stdlib.html#dates-and-times
+# Adapted from :
+# Python Tutorial 10.8. Dates and Times https://docs.python.org/3/tutorial/stdlib.html#dates-and-times
 # and https://stackoverflow.com/questions/46504954/converting-time-to-am-pm-using-python
 
+# import the date class from the datetime library so that the date functions 
+# can be used below 
 from datetime import datetime 
-
-# rightnow = datetime.now()
-
-# print(rightnow.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B."))
-# print(rightnow.strftime("%A, %B %d'something' %Y at %H:%M %p"))
 
 # Creating a function which reads an integer value and outputs the value 
 # as a string with appropriate suffix st, nd, rd or th
@@ -33,19 +31,23 @@ def nth(day):
             suffix = 'th'
     return str(day)+ suffix
 
-# dayord is todays date in the format nth e.g. 25th
+# the function strftime returns the day of todays date as string  e.g. "17"
+# the standard int function converts e.g. "17" into a integer 17
+# the custom nth function is then used to add the appropriate suffix to 
+# the string e.g. "17th" 
 dayord = nth(int(datetime.now().strftime("%d")))
+# the function strftime sets daymonth to be todays day and month e.g. "Monday, March"
 daymonth = datetime.now().strftime("%A, %B") 
+# the function strftime sets year to be todays year e.g. "2019"
 year = datetime.now().strftime("%Y") 
+# the function strftime sets time to be todays time e.g. "1.15pm"
 time = datetime.now().strftime("%-I:%M")
+# the function strftime sets ampm to be todays time as AM/PM e.g. "PM"
+# the standard function lower convert this value to lowercase e.g. "pm"
 ampm = datetime.now().strftime("%p").lower()
 
+# print to the screen todays day in the required format using an f string
 print(f"{daymonth} {dayord} {year} at {time}{ampm}")
-
-
-
-
-
 
 # 03 Mar-19 
 # 1. Note: when the python file was named datetime.py then the following error message was displayed in terminal
@@ -60,5 +62,12 @@ print(f"{daymonth} {dayord} {year} at {time}{ampm}")
 # Fionas-MacBook-Air:52445_19_asgmt fionaoriordan$ 
 
 # 03-Mar-19
-# 2. When I changed the file name from datetime.py (as specified in Problem Set 2019, Question 9) to fionadatetime.py 
-# the issue was resolved. I assume the conflict arose when the libray name datetime was used as a file name. 
+# 2. When I changed the file name from datetime.py (as specified in Problem Set 2019, 
+# Question 9) to fionadatetime.py the issue was resolved. 
+# I assume the conflict arose when the libray name datetime was used as a file name. 
+
+# 18-Mar-19
+#   3. Now I think the issue was caused because I used 'from datetime import date'
+#   instead of  'from datetime import datetime' and I also madeoverlooked this change
+#  I had also made.
+#  But I will leave the file name as is.
