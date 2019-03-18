9094a404bf0bd720990a733319b97c4548c4ef56
Fiona O'Riordan <fiona_oriordan@hotmail.com>
2019-03-18 19:43:57 +0000

    Updated comments

--- divisors.py
+++ divisors.py
@@ -1,7 +1,11 @@
 # Fiona O'Riordan, 28-FEB-19
-# Question 1 Write a program that prints all numbers between 1,000 and 10,000 
+# Module 52445, Problem Set, Question 3
+# Write a program that prints all numbers between 1,000 and 10,000 
 # that are divisible by 6 but not 12.
-# Assumption: between 1,000 and 10,000 is inclusive of both values
+# My assumption: between 1,000 and 10,000 is inclusive of both values
+# Adapted from: 
+# 1. https://docs.python.org/3.7/tutorial/controlflow.html#if-statements
+# 2. https://docs.python.org/3.7/tutorial/introduction.html#first-steps-towards-programming
 
 # set i equal to 1000 in order to begin at that value 
 i = 1000 
@@ -10,7 +14,7 @@ i = 1000
 while i <= 10000:
     # test if i is evenly divisible by 6 but not 12
     if (i%6 == 0) and (i%12 != 0):
-        # print the number that is true for both conditions to the screen 
+        # print the number if the condition is true for both conditions to the screen 
         print(i)
     # increment value of i by 1 in order to test the next number 
     i = i+1
