5d3e284d5bb78f78865e21e1185d511663ac7fa4
Fiona O'Riordan <fiona_oriordan@hotmail.com>
2019-03-18 19:13:51 +0000

    Updated the comments and simplified the code.

--- sumupto.py
+++ sumupto.py
@@ -1,31 +1,33 @@
-#Fiona O'Riordan 52445 Assigment 1 
+#Fiona O'Riordan 
 #Created 12 Feb 2019
+#Module 52445 Problem Set, Question 1 
 #Write a program that asks the user to input any positive integer 
 # and outputs the sum of all numbers between one and that number.
-#python sumupto.py
-#Please enter a positive integer: 10
-#55
 
-# 12 Feb 2019: While this solution works, it is not entirely satisfactory as:
-# 1)the standard exception message raised when handling a string entered by the
-# user need to be replaced by a more user friendly message
-# 2) and then after the float has been entered another opportunity for the user 
-# to enter a postive integer also needs to be included. 
-# 3) research is need to find if a more efficent method to handle floats, 
-# zero and negative integers could be used.
-# 4)lastly, some yet unknown requirements/ improvements may be discovered.
+# Adapted from:
+# 1. https://docs.python.org/3.7/tutorial/controlflow.html#if-statements
+# 2. 2.4.3. Formatted string literals in https://docs.python.org/3/reference/lexical_analysis.html#formatted-string-literals
 
-i = int(input('Please enter a postive integer:' ))
+# assign the value entered by the user to i 
+i = int(input('Please enter a positive integer:' ))
+# remember the original number entered.
+num = i
 
-while i < 1:
-    if i == 0:
-        i = int(input('Zero is not positive. Please enter a postive integer:' ))
-    elif i < 0:
-        i =  i = int(input('Please enter a postive integer, a number greater than 0:' ))   
+# if the number entered is not a positive integer 
+if i < 1 :
+    # then tell the user and take no more actions.
+    print(f"{i} is not a positive integer.\nPlease enter a positive integer the next time you execute this script.")
+    # Otherwise begin calculating the sum of all the numbers between one and the number entered.
+else: 
+    # set the answer = the number entered 
+    ans = i
+    # loop while the last number added to the answer is greater than or equal to 1
+    while i >= 1:
+        # increment the answer by adding a number one less than the number entered/added during the last loop
+        ans = ans + (i-1)
+        # decrease the number entered by 1
+        i = i-1
+    # print the answer
+    print(f"The sum of all the numbers between {num} and 1 is {ans}.")
 
-ans = i
-while i >= 1:
-    ans = ans + (i-1)
-    i = i-1
 
-print(ans)
