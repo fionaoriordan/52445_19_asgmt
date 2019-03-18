33c79ac782461d3e6e360841145128507e1ab7fd
Fiona O'Riordan <fiona_oriordan@hotmail.com>
2019-03-18 19:37:13 +0000

    Updated comments on collatz.py

--- collatz.py
+++ collatz.py
@@ -5,6 +5,9 @@
 # the next value by taking the current value and, if it is even, divide it by two, 
 # but if it is odd, multiply it by three and add one. 
 # Have the program end if the current value is one.
+# Adapted from: 
+# 1. https://docs.python.org/3.7/tutorial/controlflow.html#if-statements
+# 2. https://docs.python.org/3.7/tutorial/introduction.html#first-steps-towards-programming
 
 # assign the value entered by the user to i 
 i = int(input('Please enter a positive integer:' ))
@@ -19,12 +22,19 @@ while i < 1:
     elif i < 0:
         i = int(input('Not a postive integer. Please enter a positive integer:' )) 
 
-
+# loop while i is greater than one, initially i is the number entered
 while i > 1:
+    # if the current value of i is even
     if (i%2 == 0):
+        # then print the number
         print(int(i))
+        # and then divide the current value of i  by 2
         i = i/2
+    # otherwise if the current value of i number is odd
     else:
+        # then print the number
         print(int(i))
+        # but then multiply it by three and then add one.
         i = i*3 +1
+# lastly print the number which is now at 1.
 print(int(i))
