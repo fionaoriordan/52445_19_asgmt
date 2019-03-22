e1fccc1ec3d16000695a93ed81821c9e1b906026
Fiona O'Riordan <fiona_oriordan@hotmail.com>
2019-03-22 15:29:12 +0000

    Finished script and updated the comments

--- second.py
+++ second.py
@@ -8,36 +8,34 @@
 # Call me Ishmael.  Some years ago--never mind how long
 # particular to interest me on shore, I thought I would sail about a
 # ...
+# 1. https://stackoverflow.com/a/37890826
+# 2. https://stackoverflow.com/questions/1403674/pythonic-way-to-return-list-of-every-nth-item-in-a-larger-list
+# 3. https://docs.python.org/2/library/sys.html#sys.argv
+# 4. https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
-# text = input('Please enter the name of your textfile: ' )
+# import sys library so that we can use function sys.argv 
+import sys
 
-# if text[-4:] != '.txt':
-#     text = input('Please enter the name of your textfile: ' )
-# Adapted from https://stackoverflow.com/a/37890826
+# if two arguments are entered by the user i.e. the program name and the file name 
+if len(sys.argv) == 2:
+    # then the textfile name is set to be the value of the second argument entered 
+    textfile = sys.argv[1]
+
+    with open(textfile,'r+') as f1:
+        readfile = f1.read()
+        # Firstly, the readfile is split into a list with the endline marker '\n' used to indicate the separate values 
+        # Next, every second value in the list is selected
+        # Lastly, the selected values are printed as output with an endline value of '\n' placed after each value 
+        print('\n'.join(readfile.split('\n')[::2])) 
+# Otherwise the user is given advise     
+else: 
+    print('You should resubmit the program and enter a single file name')
+     
+
+  
 
 
 
-# def split_lines(text): 
-#     lines = text.split('\n')     
-#     return '\n'.join(s.strip()) 
-
-
-with open('myfilefixed.txt','r+') as f:
-    readfile = f.read()
-    print(readfile.split('\n'))
-    print(readfile.split('\r\n'))
-    print(readfile.split('\r'))
-
-# with open('plain2.txt', 'U') as infile:
-#     text = infile.read()  # Automatic ("Universal read") conversion of newlines to "\n"
-# with open('plain2.txt', 'w') as outfile:
-#     outfile.write(text)  # Writes newlines for the platform running the program
-    
-# if "\r\n" in open("/path/file.txt","rb").read():
-#     print("DOS line endings found")
-
-# s = ("Hello World\ngoodbye World\n")
-# print(s)
    
 
        
